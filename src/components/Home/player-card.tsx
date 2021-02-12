02791b52178c7a9bd32a72251854f971010f46ee
Dhruv <dhruvjobanputra8@gmail.com>
2021-02-12 14:08:10 +0800

    changed UI and added icons

--- src/components/Home/player-card.tsx
+++ src/components/Home/player-card.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import { connect } from "react-redux";
+import 'font-awesome/css/font-awesome.min.css';
+
 import {
   removePlayerAction,
   toggleActiveAction,
@@ -49,10 +51,8 @@ const PlayerCard = (props: PlayerProps) => {
       <PlayerName>{props.name}</PlayerName>
 
       <PlayerAbility>{props.ability}</PlayerAbility>
-      <br />
-      <br />
-      <button onClick={handleRemove}>Remove</button>
-      <button onClick={toggleActive}>Toggle</button>
+      <Icon className={"toggle " + (props.active ? "fa fa-check-circle btn btn-success" : "fa fa-times-circle btn btn-danger")} onClick={toggleActive}/>
+      <Icon className="remove fa fa-trash fa-2x btn btn-danger" onClick={handleRemove}/>
     </Container>
   );
 };
@@ -77,39 +77,65 @@ export default connect(null, mapDispatchToProps)(PlayerCard);
 
 const Container = styled.div`
   display: block;
-  border-radius: 20px;
-  color: white;
-  background-color: skyblue;
+  //border-radius: 20px;
+  color: black;
+  background-color: lightgrey;
 
   height: fit-content;
   word-wrap: break-word;
 
-  padding: 10px;
+  padding: 20px;
+  padding-right: 10px;
   z-index: 1000;
 
-  width: fit-content;
+  width: 100%;
+  max-width: 800px;
 
   margin: auto;
-  margin-bottom: 20px;
+
+  //margin-bottom: 20px;
+
+  //box-shadow: 0px 0px 10px 10px rgba(0,0,0,0.8);
 
   &.active {
-    background-color: green;
+    //background-color: green;
+    //background-image: linear-gradient(0deg, green 10%, lightgrey 40%);
+    border: 2px solid green;
   }
 
   &.inactive {
-    background-color: red;
+    //background-color: red;
+    //background-image: linear-gradient(0deg, red 10%, lightgrey 40%);
+    border: 2px solid red;
   }
 `;
 
 const PlayerName = styled.label`
-  float: left;
-  margin-left: 20px;
+  display: inline-block;
   margin-right: 10vw;
-  width: 80px;
+  width: 20%;
+
+  font-weight: bold;
+
+  margin-right: 5%;
+
+  word-break: break-all;
 `;
 
 const PlayerAbility = styled.label`
-  float: left;
-  margin-left: 10px;
-  width: 100px;
+  display: inline-block;
+  width: 30%;
+
+  margin-right: 20%;
+`;
+
+const Icon = styled.i`
+  display: inline;
+  padding: 1% 2%;
+  font-size: 20px;
+
+  &.toggle {
+    margin-right: 20px;
+  }
+
 `;
