a07e39520ca12c233a968d2da6cf0eb47047cbcc
Dhruv <dhruvjobanputra8@gmail.com>
2021-02-12 19:21:23 +0800

    added enum types and select tag

--- src/components/Home/list-players.tsx
+++ src/components/Home/list-players.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 
 import { selectAllPlayers } from "../../store/Players/selector";
@@ -8,18 +8,46 @@ import PlayerCard from "./player-card";
 import { Player } from "../../store/Players/actions";
 import styled from "styled-components";
 
+
+enum SortType {
+  none = "none",
+  a_first = "a to z",
+  a_last = "z to a",
+  highest_first = "high to low",
+  lowest_first = "low to high"
+
+}
 interface ListPlayersProps {
   fetchAllPlayers: () => void;
   playerList: Player[];
 }
 const ListPlayers = (props: ListPlayersProps) => {
+  const [playerList, setPlayerList] = useState(props.playerList);
+  const [sort, setSort] = useState(String(SortType.none));
+
   useEffect(() => {
     props.fetchAllPlayers();
   }, []);
 
+
+
   return (
     <React.Fragment>
       <Container>
+      <Label>Sort by: </Label>
+      <Select
+        name="sort"
+        value={sort}
+        onChange={(e) => {
+          setSort(e.target.value);
+        }}
+      >
+        <Option>{SortType.none}</Option>
+        <Option>{SortType.a_first}</Option>
+        <Option>{SortType.a_last}</Option>
+        <Option>{SortType.highest_first}</Option>
+        <Option>{SortType.lowest_first}</Option>
+      </Select>
       {props.playerList
         .map((player, id) => (
           <PlayerCard
@@ -59,4 +87,21 @@ const Container = styled.div`
   margin: auto;
   margin-bottom: 20px;
   
-`
+`;
+
+const Select = styled.select`
+  padding: 5px;
+  margin-bottom: 10px;
+  //background-color: lightgreen;
+`;
+
+const Option = styled.option`
+  //background-color: red;
+`;
+
+const Label = styled.option`
+display: inline;
+margin-left: 5%;
+margin-right: 2%;
+  //background-color: red;
+`;
